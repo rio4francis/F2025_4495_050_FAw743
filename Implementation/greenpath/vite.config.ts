7274deb1983670abc966959d5605a5da4fe4270e
rio4francis <rio4francis@gmail.com>
2025-10-24 19:28:06 -0700

    updated

--- Implementation/greenpath/vite.config.ts
+++ Implementation/greenpath/vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   base: '/',   // important for root domain hosting
+  plugins: [react(), tailwind()],
 })
